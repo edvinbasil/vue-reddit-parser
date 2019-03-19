f4b1058b25f87dafba5f05ed483cfbcfa07c3ffb
edvinbasil <edvinbasil@gmail.com>
2019-03-19 19:12:59 +0530

    Add subreddit search bar
    - Add fetch fail catch

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app" class="container mt-2">
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <form @submit.prevent="fetchReddit(url)" class=" row justify-content-end">
+            <input class="form-control" type="text" v-model="subreddit">
+            <button class="btn btn-primary" type="submit">Search</button>
+        </form>
+      </li>
+    </ul>
     <h1>{{ title }}</h1>
     <main>
       <sPost v-for="post in posts" :post="post" :key="post.data.id"></sPost>
@@ -35,6 +43,9 @@ export default {
     fetchReddit(url) {
       fetch(url)
         .then(res => res.json())
+        .catch((err) => {
+          console.log(err.message);
+        })
         .then((result) => {
           this.posts = result.data.children;
           this.title = this.posts[0].data.subreddit;
