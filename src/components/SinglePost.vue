b9ff5ba0f5ab8ed960236bae49a7372be9df59e4
edvinbasil <edvinbasil@gmail.com>
2019-03-20 14:46:01 +0530

    Fix image for non-image posts, fix post url href
    - imgUrl computed to placeholder incase its empty
    - Change post link to anchor to link easily.

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="media m-3">
-      <img :src="post.data.thumbnail" class="mr-3" alt="">
+      <img :src="imgUrl" class="mr-3" alt="">
       <div class="media-body">
         <a :href="makeLink(post.data.permalink)" target="_blank" rel="noreferrer noopener">
           <h5 class="mt-0">{{ post.data.title }}</h5>
         </a>
+        <a :href="post.data.url" class="btn btn-dark btn-sm mr-2">Link</a>
         <div class="badge badge-danger">{{ post.data.ups }} &#x2B06;</div>
         <div class="badge badge-warning">{{ post.data.subreddit_name_prefixed }}</div>
         <div class="badge badge-info">u/{{ post.data.author }}</div>
         <div class="badge">{{ post.data.num_comments }} comments</div>
-        <button style="display: block" class="btn btn-dark btn-sm">Link</button>
         <div> {{ post.data.selftext }}</div>
       </div>
     </div>
@@ -23,6 +23,11 @@ export default {
   methods: {
     makeLink(perm) {
       return `https://www.reddit.com${perm}`;
+    },
+  },
+  computed: {
+    imgUrl() {
+      return this.post.data.thumbnail === 'self' ? 'https://via.placeholder.com/160?text=No+Image+:(' : this.post.data.thumbnail;
     },
   },
 };
